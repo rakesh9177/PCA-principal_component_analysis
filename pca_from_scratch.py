003f6b8385868d9714bddef9f428511232af1f93
rakesh9177 <112565618+rakesh9177@users.noreply.github.com>
2022-12-03 23:22:11 -0500

    Update pca_from_scratch.py

--- pca_from_scratch.py
+++ pca_from_scratch.py
@@ -2,12 +2,19 @@ import numpy as np
 
 
 def pca(X,n_components=2):
-  cov = np.cov(x.T). #Getting the covariance matrix of the feature space
-  print(cov.shape)
+  #Getting the covariance matrix of the feature space
+  cov = np.cov(x.T). 
+  #get the eigen_values and eigen_vectors of the covariance matrix
+  #eigh will give eigen values and its associated eigen vectors in ascending order of eigen values
   eigen_values, eigen_vectors= np.linalg.eigh(cov)
+  
+  #for pca , we need eigen vectors associated with top n_component eigen vectors i.e for 2 components, we need eigen vectors of top 2 eigen values
   required_ev = eigen_vectors[:,-2:]
+  
+  #Normalize the eigen vector using norm, if v is a vector, it's normalized form is v/|v|
   for i in range(n_components):
     nm = np.linalg.norm(required_ev[:,i])
     required_ev[:,i]= ((1/nm)*required_ev[:,i])
-  print(required_ev.shape)
+ 
+  # returning n principal components, by default it's 2, therefore it returns two principal components, PC1 = normalized_vector.Transpose x Data.Transpose
   return np.matmul(required_ev.T,X.T)
